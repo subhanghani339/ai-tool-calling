64406818d8e72eb8ac3af178894ad5649dc1541e
Muhammad Subhan <subhanghani339@gmail.com>
2025-07-13 03:24:30 +0500

    weather API implemented

--- tool.py
+++ tool.py
@@ -1,14 +1,32 @@
-from agents import Agent, Runner
-from main import config 
+from agents import Agent, Runner, function_tool
+from main import config
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+weather_api_key=os.getenv("WEATHER_API_KEY")
+weather_api_base_url=os.getenv("BASE_URL")
+
+@function_tool
+def usd_to_pkr():
+    return "Today USD to PKR is 285."
+
+@function_tool
+def get_weather(city: str) -> str:
+    response=requests.get(f"{weather_api_base_url}?key={weather_api_key}&q={city}")
+    data=response.json()
+    return f"The current weather in {city} is {data['current']['temp_c']}C with {data['current']['condition']['text']}."
 
 agent = Agent(
     name="General Agent",
-    instructions="You are a helpful assisstant. Your task is to help the user with their queries."
+    instructions="You are a helpful assisstant. Your task is to help the user with their queries.",
+    tools=[usd_to_pkr, get_weather]
 )
 
 result = Runner.run_sync(
     agent,
-    'Who is the founder of pakistan?',
+    'What is the current weather in Karachi today?',
     run_config=config
 )
 
